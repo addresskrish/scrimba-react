ca20faa195d9e46a5cb515fbc46b74bfee4cef96
KRISH <addresskrish@gmail.com>
2025-06-10 23:29:33 +0530

    Update 02-travel-journal

--- 02-travel-journal/src/App.jsx
+++ 02-travel-journal/src/App.jsx
@@ -1,25 +1,29 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Entry from './components/Entry'
-import JapanImg from "../src/assets/images/japan.jpeg"
+import datas from '../data'
 import './App.css'
 
 function App() {
 
+  const dataItem = datas.map((data) => {
+    return (
+      <Entry 
+        id={ data.id }
+        img={ data.img }
+        title={ data.title }
+        country={ data.country }
+        googleMapsLink={ data.googleMapsLink }
+        dates={ data.dates }
+        text={ data.text }
+      />
+    )
+  })
+
   return (
     <>
       <Header />
-      <Entry 
-        img={{
-           src: JapanImg, 
-           alt: "Japan-img" 
-        }}
-        name="JAPAN"
-        link="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-        mainname="Mount Fuzi"
-        date="12, Jan, 2023 - 24 Jan, 2023"
-        description="Mount Fuzi is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-      />
+      { dataItem }
     </>
   )
 }
--- 02-travel-journal/src/components/Entry.jsx
+++ 02-travel-journal/src/components/Entry.jsx
@@ -1,7 +1,7 @@
 import MarkerLogo from "../assets/images/marker.png"
 
 function Entry(props) {
-    const { img, name, link, mainname, date, description } = props
+    const { id, img, country, googleMapsLink, title, dates, text } = props
     return (
         <>
             <main>
@@ -11,17 +11,17 @@ function Entry(props) {
                 <div className="main-2">
                     <div className="main-2-1">
                         <img src={ MarkerLogo } alt="marker-img" width="15px"/>
-                        <p className="name">{ name }</p>
-                        <a href={ link }>View on Google Maps</a>
+                        <p className="name">{ country }</p>
+                        <a href={ googleMapsLink }>View on Google Maps</a>
                     </div>
                     <div className="main-2-2">
-                        <h1>{ mainname }</h1>
+                        <h1>{ title }</h1>
                     </div>
                     <div className="main-2-3">
-                        <p className="date">{ date }</p>
+                        <p className="date">{ dates }</p>
                     </div>
                     <div className="main-2-4">
-                        <p>{ description }</p>
+                        <p>{ text }</p>
                     </div>
                 </div>
             </main>
