9d4a7c99c75faa684578f633be68803cdbafa247
KRISH <addresskrish@gmail.com>
2025-06-19 02:43:00 +0530

    chef cluade main part created

---
+++ 03-chef-claude/src/components/Main.jsx
@@ -0,0 +1,12 @@
+function Main() {
+    return (
+            <main>
+                <form action="">
+                    <input type="text" placeholder="e.g. oregano" />
+                    <button>Add ingredient</button>
+                </form>
+            </main>
+    )
+}
+
+export default Main
